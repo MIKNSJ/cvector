1dd23680df07e3664b9e85044f804f900bdf21f3
MIKNSJ <sculetoncode@gmail.com>
2024-04-30 12:03:12 -0700

    init files

---
+++ src/test.c
@@ -0,0 +1,20 @@
+/*==============================================================================
+||                                                                            ||
+||  2024-04-30                                                                ||
+||  Purpose: Tests the implmentation of the vector library                    ||
+||                                                                            ||
+==============================================================================*/
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ * The main function
+ * @return 0 for success, 1 otherwise
+*/
+int main() {
+    return 0;
+}
